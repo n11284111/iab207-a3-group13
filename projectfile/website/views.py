36664855be8ee1b9c41ef537d664bf1e77676115
Viahn Pulikottil <n11284111@qut.edu.au>
2023-06-07 18:47:53 +1000

    Update views.py
    Altered search function and cleaned up file

--- projectfile/website/views.py
+++ projectfile/website/views.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from .models import Destination
+from .models import Event
 from . import db
 
 mainbp = Blueprint('main', __name__)
 
 @mainbp.route('/')
-def index():
-    destinations = db.session.scalars(db.select(Destination)).all()    
-    return render_template('index.html', destinations=destinations)
+def index():  
+    events = db.session.scalars(db.select(Event)).all()  
+    return render_template('index.html', events=events)
 
 @mainbp.route('/search', methods =["GET", "POST"])
 def search():
@@ -17,7 +17,7 @@ def search():
         if searchval and searchval != "":
             print(searchval)
             query = "%" + searchval + "%"
-            destinations = db.session.query(Destination).filter(Destination.description.contains(query))
-            return render_template('index.html', destinations=destinations)
+            events = db.session.query(Event).filter(Event.name.contains(query))
+            return render_template('index.html', events=events)
         else:
             return redirect(url_for('main.index'))
